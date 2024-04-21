db8e8d04cb45e1f3dde5abbde2bf85e2c0449c35
tomrajda <eu.crafciarze@gmail.com>
2024-04-21 10:46:30 +0200

    inital commit

---
+++ src/App2.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import { airports, flights } from '@2bad/ryanair';
+
+const dates = flights.getDates('BER', 'DUB')
+
+
+</script>
+
+
+
+
+<template>
+    
+    <div>
+        {{ dates }}
+    </div>
+
+</template>
